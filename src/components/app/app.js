fd043c35c535ef9f2f9a7600fed3856846a9d0bc
sunstanislav <sundeev91@ya.ru>
2022-09-25 20:32:22 +0600

    практика поиск

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,8 @@ class App extends Component {
                 {name: 'John C.', salary: 800, increase: false, rise:true, id: 1},
                 {name: 'Alex M.', salary: 3000, increase: true, rise:false, id: 2},
                 {name: 'Carl W.', salary: 5000, increase: false, rise:false, id: 3}
-            ]
+            ],
+            term: ''
         }
         this.maxId = 4;
     }
@@ -67,25 +68,40 @@ class App extends Component {
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
     render() {
+        const {data, term} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length; //возврат массива после фильтра
                                                                                 //передача сотрудников в компонет
+        const visibleData = this.searchEmp(data, term);
         return (
             <div className="app"> 
                 <AppInfo employees={employees} increased={increased} />
                 <div className="search-panel">
-                    <SearchPanel/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
                     <AppFilter/>
                 </div>
                 
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
             </div>
-        );
+        );  
     }
 }
 
